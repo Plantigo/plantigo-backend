df59b4000d528ccdeec8c51403399908096699a2
kwiats <pawuskwiatkowski@gmail.com>
2024-10-31 14:58:10 +0100

    dodano nowe zminne do config

--- telemetry/config.py
+++ telemetry/config.py
@@ -1,12 +1,10 @@
-# Konfiguracja MongoDB
 MONGO_URI = "mongodb://localhost:27017/"
 
-# Konfiguracja MQTT
 MQTT_BROKER = "192.168.0.52"
 MQTT_PORT = 1883
 MQTT_USERNAME = 'test'
 MQTT_PASSWORD = 'test'
-
+MQTT_CLIENT_IT = 'telemetry-api'
 
 ALLOWED_ORIGINS = [
     "http://localhost",
