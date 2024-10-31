8a617b85f6b5d66b305a8750f407533ad978eacc
kwiats <pawuskwiatkowski@gmail.com>
2024-10-31 15:04:45 +0100

    przeniosne z config to settings dane

--- telemetry/models.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-
-class DeviceData(BaseModel):
-    mac_address: str
-    sensor1: float
-    sensor2: float
-    sensor3: float
-    sensor4: float
-    timestamp: float
