b3f3a6596253b85286c7817ad582f76b38761d23
kwiats <pawuskwiatkowski@gmail.com>
2024-11-15 16:18:14 +0100

    #24 - usunieto listener

--- iot_listener/tests/test_db.py
+++
@@ -1,6 +0,0 @@
-from app.db.mongodb import MongoDB
-
-def test_mongodb_save():
-    db = MongoDB("mongodb://localhost:27017", "test_db")
-    result = db.save({"test": "data"})
-    assert result.inserted_id is not None
