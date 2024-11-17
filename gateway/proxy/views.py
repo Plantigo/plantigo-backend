82f3ce957cb210ff1c236bb376a4af6d5d4b1121
jaqb8 <jakubaniszewski@pm.me>
2024-11-17 19:07:19 +0100

    #54 - proto serializer

--- gateway/proxy/views.py
+++ gateway/proxy/views.py
@@ -1,18 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
-from proxy.grpc_caller import GRPCServiceFactory
-from google.protobuf.json_format import MessageToDict
-
-DevicesGRPCService = GRPCServiceFactory.create_service_class("devices")
+from proxy.services import DevicesGRPCService
+from proxy.serializers import DeviceProtoSerializer
 
 
 @api_view()
 def get_devices(request):
+    """
+    Retrieves a list of devices from the gRPC service.
+
+    Args:
+        request: The HTTP request object.
+
+    Returns:
+        Response: A DRF Response object containing the serialized list of devices or an error message.
+    """
     try:
         devices_service = DevicesGRPCService(request)
         response = devices_service.get_all_devices()
-        device_list = [MessageToDict(data) for data in response.devices]
-        return Response({'devices': device_list})
+        serializer = DeviceProtoSerializer(response.devices, many=True)
+        return Response({'devices': serializer.data}, status=200)
     except Exception as e:
-        return Response({'error': str(e)}, status=500)
+        return Response({'error': 'Server error'}, status=500)
