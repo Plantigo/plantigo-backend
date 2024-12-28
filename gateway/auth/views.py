87ff1dd01e711c0886c450e66556a79f7708fe0e
jaqb8 <jakubaniszewski@pm.me>
2024-12-28 22:58:30 +0100

    Add userinfo endpoint to retrieve authenticated user details
    Introduced a new `userinfo` endpoint in the user viewset, accessible only to authenticated users with JWT authentication. This endpoint returns data about the currently authenticated user based on the JWT token provided.

--- gateway/auth/views.py
+++ gateway/auth/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from django.contrib.auth import get_user_model
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.views import APIView, Response, status
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-
+from rest_framework.decorators import action
+from rest_framework_simplejwt.authentication import JWTAuthentication
 from .serializers import UserSerializer, RegistrationSerializer
 
 
@@ -11,14 +12,30 @@ class UserViewSet(viewsets.ModelViewSet):
     """
     A viewset for viewing and editing user instances.
     """
+
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
+
+    @action(
+        detail=False,
+        methods=['get'],
+        permission_classes=[IsAuthenticated],
+        authentication_classes=[JWTAuthentication],
+    )
+    def userinfo(self, request):
+        """
+        Retrieve info about the current authenticated user based on user_id from JWT token.
+        """
+        user = request.user
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
 
 
 class CustomTokenObtainPairView(TokenObtainPairView):
     """
     A view that provides the ability to obtain a new JWT token pair.
     """
+
     permission_classes = [AllowAny]
 
 
@@ -26,6 +43,7 @@ class CustomTokenRefreshView(TokenRefreshView):
     """
     A view that provides the ability to refresh an existing JWT token.
     """
+
     pass
 
 
@@ -33,6 +51,7 @@ class RegistrationView(APIView):
     """
     A view that allows new users to register.
     """
+
     permission_classes = [AllowAny]
 
     def post(self, request, *args, **kwargs):
@@ -41,6 +60,6 @@ class RegistrationView(APIView):
             user = serializer.save()
             return Response(
                 {"message": "User registered successfully."},
-                status=status.HTTP_201_CREATED
+                status=status.HTTP_201_CREATED,
             )
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
